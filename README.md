2bd5d32e8905f27eec31fbff576208090884a4f1
MK Kim <mk@MKs-MacBook-Pro.local>
2019-09-05 17:54:40 -0600

    Added Customer.js

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## User Management System
+This project was created to help admin to manage users in more simple way.
+
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO REACT!
-          Welcome to the first project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const users = [
+  {
+    id: 1,
+    image: 'https://source.unsplash.com/random/100x64',
+    name: 'MK',
+    gender: 'Male',
+    job: 'Dev'
+  },
+  {
+    id: 2,
+    image: 'https://source.unsplash.com/random/100x64',
+    name: 'Kim',
+    gender: 'Male',
+    job: 'Jr.Dev'
+  },
+  {
+    id: 3,
+    image: 'https://source.unsplash.com/random/100x64',
+    name: 'Hash',
+    gender: 'Female',
+    job: 'None'
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {users.map(user=>{ return (<Customer key={user.id} id={user.id} image={user.image} name={user.name} gender={user.gender} job={user.job}/>)})}
+
+      </div>
+    );
+  }
 }
 
 export default App;
